ad905bcdce66bee70eaac5c1431f72c182a4e21f
Thanh Nguyen <thanhnguyen2187@gmail.com>
2022-08-30 23:43:08 +0700

    Implement and improve various things in `DecodeDSON`

--- dson/decode.go
+++ dson/decode.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 
+	"darkest-savior/ds"
 	"github.com/pkg/errors"
 
 	"github.com/samber/lo"
@@ -36,7 +37,7 @@ type (
 	}
 	Meta1Block struct {
 		ParentIndex       int `json:"parent_index"`
-		Meta2EntryIDx     int `json:"meta_2_entry_idx"`
+		Meta2EntryIndex   int `json:"meta_2_entry_index"`
 		NumDirectChildren int `json:"num_direct_children"`
 		NumAllChildren    int `json:"num_all_children"`
 	}
@@ -47,14 +48,27 @@ type (
 		Inferences Meta2BlockInferences `json:"inferences"`
 	}
 	Meta2BlockInferences struct {
-		IsPrimitive     bool `json:"is_primitive"`
-		FieldNameLength int  `json:"field_name_length"`
-		Meta1EntryIndex int  `json:"meta_1_entry_index"`
-		RawDataLength   int  `json:"raw_data_length"`
+		IsObject          bool `json:"is_object"`
+		ParentIndex       int  `json:"parent_index"`
+		FieldNameLength   int  `json:"field_name_length"`
+		Meta1EntryIndex   int  `json:"meta_1_entry_index"`
+		NumDirectChildren int  `json:"num_direct_children"`
+		RawDataLength     int  `json:"raw_data_length"`
 	}
 	Field struct {
-		Name    string `json:"name"`
-		RawData []byte `json:"raw_data"`
+		Name       string          `json:"name"`
+		RawData    []byte          `json:"raw_data"`
+		Inferences FieldInferences `json:"inferences"`
+	}
+	FieldInferences struct {
+		IsObject        bool     `json:"is_object"`
+		ParentIndex     int      `json:"parent_index"`
+		HierarchyPath   []string `json:"hierarchy_path"`
+		RawDataOffset   int      `json:"raw_data_offset"`
+		RawDataLength   int      `json:"raw_data_length"`
+		RawDataStripped []byte   `json:"raw_data_stripped"`
+		DataType        string   `json:"data_type"`
+		Data            any      `json:"data"`
 	}
 	ReadingInstruction struct {
 		Key          string
@@ -106,7 +120,12 @@ func createIntReadFunction(reader *BytesReader) func() (any, error) {
 
 func createStringReadFunction(reader *BytesReader, n int) func() (any, error) {
 	return func() (any, error) {
-		return reader.ReadString(n)
+		result, err := reader.ReadString(n)
+		if err != nil {
+			return "", err
+		}
+		// zero byte trimming is needed since that is how strings are laid out in a DSON file
+		return strings.TrimRight(result, "\u0000"), nil
 	}
 }
 
@@ -184,7 +203,7 @@ func DecodeMeta1Block(reader *BytesReader) (*Meta1Block, error) {
 
 	meta1Instructions := []ReadingInstruction{
 		{"parent_index", readInt},
-		{"meta_2_entry_idx", readInt},
+		{"meta_2_entry_index", readInt},
 		{"num_direct_children", readInt},
 		{"num_all_children", readInt},
 	}
@@ -216,7 +235,7 @@ func DecodeMeta1Blocks(reader *BytesReader, numMeta1Entries int) ([]Meta1Block, 
 
 func InferUsingFieldInfo(fieldInfo int) Meta2BlockInferences {
 	inferences := Meta2BlockInferences{
-		IsPrimitive:     (fieldInfo & 0b1) == 1,
+		IsObject:        (fieldInfo & 0b1) == 1,
 		FieldNameLength: (fieldInfo & 0b11111111100) >> 2,
 		Meta1EntryIndex: (fieldInfo & 0b1111111111111111111100000000000) >> 11,
 	}
@@ -226,6 +245,164 @@ func InferUsingFieldInfo(fieldInfo int) Meta2BlockInferences {
 
 func InferRawDataLength(secondOffset int, firstOffset int, firstFieldNameLength int) int {
 	return secondOffset - (firstOffset + firstFieldNameLength)
+}
+
+func InferUsingMeta2Block(
+	inferences FieldInferences,
+	rawData []byte,
+	meta2block Meta2Block,
+) FieldInferences {
+	rawDataOffset := meta2block.Offset + meta2block.Inferences.FieldNameLength
+	rawDataLength := meta2block.Inferences.RawDataLength
+	alignedBytesCount := ds.NearestDivisibleByM(rawDataOffset, 4) - rawDataOffset
+	rawDataStripped := rawData
+	if rawDataLength > alignedBytesCount {
+		rawDataStripped = rawData[alignedBytesCount:]
+	}
+
+	inferences.RawDataOffset = rawDataOffset
+	inferences.RawDataLength = rawDataLength
+	inferences.RawDataStripped = rawDataStripped
+
+	return inferences
+}
+
+func InferNumDirectChildren(meta1Blocks []Meta1Block, meta2Blocks []Meta2Block) ([]Meta2Block, error) {
+	// TODO: improve the function by replacing meta1Blocks with meta2EntryIndexes
+	meta2BlocksCopy := make([]Meta2Block, len(meta2Blocks))
+	copy(meta2BlocksCopy, meta2Blocks)
+
+	for i, meta1Block := range meta1Blocks {
+		meta2EntryIndex := meta1Block.Meta2EntryIndex
+		meta2Block := &meta2BlocksCopy[meta2EntryIndex]
+		meta1EntryIndex := meta2Block.Inferences.Meta1EntryIndex
+		if !meta2Block.Inferences.IsObject {
+			err := fmt.Errorf("InferParentIndex metaBlock2 %d is not an object", meta2EntryIndex)
+			return nil, err
+		}
+		if meta1EntryIndex != i {
+			err := fmt.Errorf(
+				"InferParentIndex invalid meta1EntryIndex of meta2Block %d: expected %d; got %d",
+				meta2EntryIndex, i, meta1EntryIndex,
+			)
+			return nil, err
+		}
+		meta2Block.Inferences.NumDirectChildren = meta1Block.NumDirectChildren
+	}
+
+	return meta2BlocksCopy, nil
+}
+
+func InferParentIndex(meta2Blocks []Meta2Block) []Meta2Block {
+	meta2BlocksCopy := make([]Meta2Block, len(meta2Blocks))
+	copy(meta2BlocksCopy, meta2Blocks)
+
+	// As the fields in a DSON file are laid sequentially,
+	// a stack can be used to find out the parent index of each field.
+	//
+	// For example, visualizing a stack like this:
+	//
+	//   [{"index": 0, "num_direct_children": 3}
+	//    {"index": 1, "num_direct_children": 0}
+	//    {"index": 2, "num_direct_children": 2}
+	//    {"index": 3, "num_direct_children": 0}
+	//    {"index": 4, "num_direct_children": 0}
+	//    {"index": 5, "num_direct_children": 0}]
+	//
+	// Means the hierarchy looks like this:
+	//
+	//   0 --> 1
+	//    \--> 2 --> 3
+	//    |     \--> 4
+	//    \--> 5
+	type Tracker struct {
+		Index             int
+		NumDirectChildren int
+	}
+	stack := ds.NewStack[Tracker]()
+	stack.Push(
+		Tracker{
+			Index:             -1,
+			NumDirectChildren: 1,
+		},
+	) // set a "default" first item to reduce the complexity of edge case handling
+	for i := range meta2BlocksCopy {
+		meta2Block := &meta2BlocksCopy[i]
+		last := stack.Peek()
+		meta2Block.Inferences.ParentIndex = last.Index
+		stack.ReplaceLast(
+			func(oldTracker Tracker) Tracker {
+				oldTracker.NumDirectChildren -= 1
+				return oldTracker
+			},
+		)
+		last = stack.Peek()
+		if last.NumDirectChildren == 0 {
+			stack.Pop()
+		}
+		if meta2Block.Inferences.IsObject {
+			stack.Push(
+				Tracker{
+					Index:             i,
+					NumDirectChildren: meta2Block.Inferences.NumDirectChildren,
+				},
+			)
+		}
+	}
+
+	return meta2BlocksCopy
+}
+
+func InferRawDataLengths(meta2Blocks []Meta2Block, headerDataLength int) ([]Meta2Block, error) {
+	n := len(meta2Blocks)
+	// RawDataLength of each meta2Block is inferred by the difference between
+	//
+	// - The second block's offset, and
+	// - Sum of the first block's offset and the field name length
+	meta2BlocksCopy := lo.Map(
+		lo.Zip2(
+			meta2Blocks[:n-1],
+			meta2Blocks[1:],
+		),
+		func(t lo.Tuple2[Meta2Block, Meta2Block], _ int) Meta2Block {
+			rawDataLength := InferRawDataLength(
+				t.B.Offset,
+				t.A.Offset,
+				t.A.Inferences.FieldNameLength,
+			)
+			t.A.Inferences.RawDataLength = rawDataLength
+			return t.A
+		},
+	)
+	meta2Block, found := lo.Find(
+		meta2BlocksCopy,
+		func(meta2Block Meta2Block) bool {
+			return meta2Block.Inferences.RawDataLength < 0
+		},
+	)
+	if found {
+		err := fmt.Errorf(
+			`InferRawDataLength meta 2 block "%s" has negative raw data length`,
+			ds.JSONDumps(meta2Block),
+		)
+		return nil, err
+	}
+
+	// lastBlock is an edge case, where within calculation,
+	// it is the first block itself; data length of header serves as the second block's offset
+	lastBlock, err := lo.Last(meta2Blocks)
+	if err != nil {
+		msg := fmt.Sprintf("InferRawDataLengths unreachable code where there is no meta 2 block")
+		return nil, errors.New(msg)
+	}
+	lastBlock.Inferences.RawDataLength = InferRawDataLength(
+		headerDataLength,
+		lastBlock.Offset,
+		lastBlock.Inferences.FieldNameLength,
+	)
+	meta2BlocksCopy = append(meta2BlocksCopy, lastBlock)
+
+	return meta2BlocksCopy, nil
 }
 
 func DecodeMeta2Block(reader *BytesReader) (*Meta2Block, error) {
@@ -246,9 +423,9 @@ func DecodeMeta2Block(reader *BytesReader) (*Meta2Block, error) {
 	return meta2Block, nil
 }
 
-func DecodeMeta2Blocks(reader *BytesReader, headerDataLength int, numMeta2Entries int) ([]Meta2Block, error) {
-	meta2Blocks := make([]Meta2Block, 0, numMeta2Entries)
-	for i := 0; i < numMeta2Entries; i++ {
+func DecodeMeta2Blocks(reader *BytesReader, header Header, meta1Blocks []Meta1Block) ([]Meta2Block, error) {
+	meta2Blocks := make([]Meta2Block, 0, header.NumMeta2Entries)
+	for i := 0; i < header.NumMeta2Entries; i++ {
 		meta2Block, err := DecodeMeta2Block(reader)
 		if err != nil {
 			err := errors.Wrap(err, "DecodeMeta2Blocks error")
@@ -260,50 +437,44 @@ func DecodeMeta2Blocks(reader *BytesReader, headerDataLength int, numMeta2Entrie
 		meta2Blocks = append(meta2Blocks, *meta2Block)
 	}
 
-	// RawDataLength of each meta2Block is inferred by the difference between
-	//
-	// - The second block's offset, and
-	// - Sum of the first block's offset and the field name length
-	//
-	// There is an exception for the last block. As there is no second block, header's data length (the whole file's
-	// length in bytes) is used as the second block's offset, and field name length is also zero in this case.
-	for i := 1; i < numMeta2Entries-1; i++ {
-		rawDataLength := InferRawDataLength(
-			meta2Blocks[i].Offset,
-			meta2Blocks[i-1].Offset,
-			meta2Blocks[i-1].Inferences.FieldNameLength,
-		)
-		if rawDataLength < 0 {
-			//goland:noinspection GoErrorStringFormat
-			err := fmt.Errorf("DecodeMeta2Blocks cannot have negative raw data length %d", rawDataLength)
-			return nil, err
-		}
-		meta2Blocks[i].Inferences.RawDataLength = InferRawDataLength(
-			meta2Blocks[i+1].Offset,
-			meta2Blocks[i].Offset,
-			meta2Blocks[i].Inferences.FieldNameLength,
-		)
+	meta2Blocks, err := InferRawDataLengths(meta2Blocks, header.DataLength)
+	if err != nil {
+		err := errors.Wrap(err, "DecodeMeta2Blocks error")
+		return nil, err
 	}
-	lastBlock := meta2Blocks[numMeta2Entries-1]
-	lastBlock.Inferences.RawDataLength = InferRawDataLength(
-		headerDataLength,
-		lastBlock.Offset,
-		0,
-	)
+	meta2Blocks, err = InferNumDirectChildren(meta1Blocks, meta2Blocks)
+	if err != nil {
+		err := errors.Wrap(err, "DecodeMeta2Blocks error")
+		return nil, err
+	}
+	meta2Blocks = InferParentIndex(meta2Blocks)
 
 	return meta2Blocks, nil
 }
 
 func DecodeField(reader *BytesReader, meta2Block Meta2Block) (*Field, error) {
-
 	// manual decoding and mapping is needed since turning data into JSON
 	// and parse back does not work for bytes of RawData
-	field := Field{}
+	field := Field{
+		Inferences: FieldInferences{
+			IsObject:      meta2Block.Inferences.IsObject,
+			ParentIndex:   meta2Block.Inferences.ParentIndex,
+			RawDataLength: meta2Block.Inferences.RawDataLength,
+			RawDataOffset: meta2Block.Offset + meta2Block.Inferences.FieldNameLength,
+		},
+	}
 	err := error(nil)
-	field.Name, err = reader.ReadString(meta2Block.Inferences.FieldNameLength)
-	field.Name = strings.Trim(field.Name, "\u0000")
+	readString := createStringReadFunction(reader, meta2Block.Inferences.FieldNameLength)
+	ok := false
+
+	fieldName, err := readString()
 	if err != nil {
-		err := errors.Wrap(err, "DecodeField error")
+		err := errors.Wrap(err, "DecodeField error: read field.Name")
+		return nil, err
+	}
+	field.Name, ok = fieldName.(string)
+	if !ok {
+		err := fmt.Errorf(`DecodeField error: unable to cast value "%v" to string for field.Name`, fieldName)
 		return nil, err
 	}
 	hashed := HashString(field.Name)
@@ -314,13 +485,41 @@ func DecodeField(reader *BytesReader, meta2Block Meta2Block) (*Field, error) {
 		)
 		return nil, err
 	}
+
 	field.RawData, err = reader.ReadBytes(meta2Block.Inferences.RawDataLength)
 	if err != nil {
-		err := errors.Wrap(err, "DecodeField error")
+		err := errors.Wrap(err, "DecodeField error: read field.RawData")
 		return nil, err
 	}
 
+	field.Inferences = InferUsingMeta2Block(
+		field.Inferences,
+		field.RawData,
+		meta2Block,
+	)
+
 	return &field, nil
+}
+
+func InferHierarchyPath(index int, fields []Field) []string {
+	// TODO: create a cache function later
+	fieldName := fields[index].Name
+	parentIndex := fields[index].Inferences.ParentIndex
+	if parentIndex == -1 {
+		return []string{fields[index].Name}
+	}
+	return append(InferHierarchyPath(parentIndex, fields), fieldName)
+}
+
+func InferHierarchyPaths(fields []Field) []Field {
+	fieldsCopy := lo.Map(
+		fields,
+		func(t Field, i int) Field {
+			t.Inferences.HierarchyPath = InferHierarchyPath(i, fields)
+			return t
+		},
+	)
+	return fieldsCopy
 }
 
 func DecodeFields(reader *BytesReader, meta2Blocks []Meta2Block) ([]Field, error) {
@@ -333,6 +532,8 @@ func DecodeFields(reader *BytesReader, meta2Blocks []Meta2Block) ([]Field, error
 		}
 		fields = append(fields, *field)
 	}
+
+	fields = InferHierarchyPaths(fields)
 
 	return fields, nil
 }
@@ -351,7 +552,7 @@ func DecodeDSON(reader *BytesReader) (*DecodedFile, error) {
 		return nil, err
 	}
 
-	file.Meta2Blocks, err = DecodeMeta2Blocks(reader, header.DataLength, header.NumMeta2Entries)
+	file.Meta2Blocks, err = DecodeMeta2Blocks(reader, file.Header, file.Meta1Blocks)
 	if err != nil {
 		return nil, err
 	}
