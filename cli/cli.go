ded13f050f32a7a5f3a1a9ba873f7c43c8de3e46
Thanh Nguyen <thanhnguyen2187@gmail.com>
2022-09-04 11:55:58 +0700

    Implement CLI frontend to convert from DSON to JSON

---
+++ cli/cli.go
@@ -0,0 +1,105 @@
+package cli
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"darkest-savior/dson"
+	"darkest-savior/dson/dheader"
+	"github.com/alexflint/go-arg"
+	"github.com/pkg/errors"
+)
+
+type (
+	Args struct {
+		Interactive *InteractiveCmd `arg:"subcommand:interactive"`
+		Convert     *ConvertCmd     `arg:"subcommand:convert"`
+	}
+	InteractiveCmd struct{}
+	ConvertCmd     struct {
+		// TODO: improve UX of `from` and `to`
+		// the underlying library has some limitation on displaying help and placeholder
+		// too long placeholder force help to be put on another line, which looks really ugly
+		// that is why text is really sparse for the arguments, even though I wanted it to be clearer
+		From  string `arg:"required" help:"path to source file" placeholder:"persist.json"`
+		To    string `arg:"required" help:"path to destination file" placeholder:"file.json"`
+		Force bool   `help:"overwrite the destination file"`
+	}
+)
+
+func (Args) Description() string {
+	des := strings.Join(
+		[]string{
+			"Ruin has come to our command line.\n",
+			"A CLI utility to convert DSON (Darkest Dungeon's own proprietary JSON format)",
+			"to \"standard\" JSON in the command line.",
+		},
+		"\n",
+	)
+	des += "\n"
+	return des
+}
+
+func StartInteractive() {
+	print("Not implemented!")
+}
+
+func CheckExistence(path string) bool {
+	_, err := os.Stat(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return err == nil
+}
+
+func StartConverting(from string, to string, force bool) {
+	if !CheckExistence(from) {
+		println("Source file does not exist!")
+		return
+	}
+	if CheckExistence(to) && !force {
+		println("Destination file existed. Please type the command again with --force to allow overwriting!")
+		println("Explicit --force is needed to make sure that you paid attention not to overwriting the actual DSON file in your folder.")
+		return
+	}
+	fileBytes, err := ioutil.ReadFile(from)
+	if err != nil {
+		println("Error happened reading file")
+		return
+	}
+
+	if dheader.IsValidMagicNumber(fileBytes[:4]) {
+		decodedFile, err := dson.DecodeDSON(fileBytes)
+		if err != nil {
+			println("Error happened decoding DSON to JSON")
+			return
+		}
+		decodedMap := dson.ToLinkedHashMap(*decodedFile)
+		decodedBytes, err := json.MarshalIndent(decodedMap, "", "  ")
+		if err := ioutil.WriteFile(to, decodedBytes, 0644); err != nil {
+			println("Error happened writing to file at: " + to)
+			return
+		}
+		println("Done converting. Please check your result file at: " + to)
+	} else {
+		println("Not implemented")
+	}
+}
+
+func Start() {
+	args := Args{}
+	arg.MustParse(&args)
+
+	if (args.Interactive == nil && args.Convert == nil) ||
+		args.Interactive != nil {
+		StartInteractive()
+	} else {
+		StartConverting(
+			args.Convert.From,
+			args.Convert.To,
+			args.Convert.Force,
+		)
+	}
+}
