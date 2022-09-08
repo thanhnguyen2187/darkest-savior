33f546bb08199df9df7c71b08a18d11254c695cb
Thanh Nguyen <thanhnguyen2187@gmail.com>
2022-09-08 08:00:37 +0700

    Implement `dfield.Encode` functions

--- dson/dfield/encode.go
+++ dson/dfield/encode.go
@@ -2,7 +2,10 @@ package dfield
 
 import (
 	"encoding/binary"
+	"fmt"
 	"math"
+
+	"github.com/samber/lo"
 )
 
 func EncodeValueBool(value any) []byte {
@@ -21,13 +24,16 @@ func EncodeValueChar(value any) []byte {
 
 func EncodeValueInt(value any) []byte {
 	// TODO: research on potential errors of this type of handling
-	valueFloat64, ok := value.(float64)
 	valueUInt32 := uint32(0)
-	if ok {
+	switch value.(type) {
+	case float64:
+		valueFloat64 := value.(float64)
 		valueUInt32 = uint32(valueFloat64)
-	} else {
+	case int:
 		valueInt := value.(int)
 		valueUInt32 = uint32(valueInt)
+	case uint32:
+		valueUInt32 = value.(uint32)
 	}
 	bs := make([]byte, 4)
 	binary.LittleEndian.PutUint32(bs, valueUInt32)
@@ -39,7 +45,7 @@ func EncodeValueFloat(value any) []byte {
 	valueFloat64 := value.(float64)
 	valueFloat32 := float32(valueFloat64)
 	valueUInt32 := math.Float32bits(valueFloat32)
-	bs := make([]byte, 0, 4)
+	bs := make([]byte, 4)
 	binary.LittleEndian.PutUint32(bs, valueUInt32)
 	return bs
 }
@@ -54,41 +60,82 @@ func EncodeValueString(value any) []byte {
 
 func EncodeValueIntVector(value any) []byte {
 	valueFloat64Vector := value.([]float64)
+	bs := EncodeValueInt(len(valueFloat64Vector))
+	lo.Reduce(
+		valueFloat64Vector,
+		func(bs []byte, valueFloat64 float64, i int) []byte {
+			return append(bs, EncodeValueInt(valueFloat64)...)
+		},
+		bs,
+	)
+	return bs
 }
 
-func EncodeValue(valueType DataType, value any) []byte {
-	type EncodeFunc func(any) []byte
-	dispatchMap := map[DataType]EncodeFunc{
-		DataTypeBool:   EncodeValueBool,
-		DataTypeChar:   EncodeValueChar,
-		DataTypeInt:    EncodeValueInt,
-		DataTypeFloat:  EncodeValueFloat,
-		DataTypeString: EncodeValueString,
-		// DataTypeIntVector = DataType("int_vector")
-		// DataTypeFloatVector = DataType("float_vector")
-		// DataTypeStringVector = DataType("string_vector")
-	}
-	switch valueType {
-	case DataTypeUnknown:
-		return nil
-	case DataTypeBool:
-		valueBool := value.(bool)
-		if valueBool {
-			return []byte{1}
-		} else {
-			return []byte{0}
-		}
-	case DataTypeChar:
-		valueStr := value.(string)
-		return []byte{valueStr[0]}
-	case DataTypeInt:
-		valueFloat64 := value.(float64)
-		valueInt32 := uint32(valueFloat64)
-		bs := make([]byte, 0, 4)
-		binary.LittleEndian.PutUint32(bs, valueInt32)
-		return bs
-	case DataTypeFloat:
-	}
+func EncodeValueFloatVector(value any) []byte {
+	valueFloat64Vector := value.([]float64)
+	bs := EncodeValueInt(len(valueFloat64Vector))
+	lo.Reduce(
+		valueFloat64Vector,
+		func(bs []byte, valueFloat64 float64, i int) []byte {
+			return append(bs, EncodeValueFloat(valueFloat64)...)
+		},
+		bs,
+	)
+	return bs
+}
 
-	return nil
+func EncodeValueStringVector(value any) []byte {
+	valueStringVector := value.([]string)
+	bs := EncodeValueInt(len(valueStringVector))
+	lo.Reduce(
+		valueStringVector,
+		func(bs []byte, valueStr string, i int) []byte {
+			return append(bs, EncodeValueString(valueStr)...)
+		},
+		bs,
+	)
+	return bs
+}
+
+type ErrNoEncodeFunc struct {
+	Key       string
+	ValueType DataType
+	Value     any
+}
+
+func (r ErrNoEncodeFunc) Error() string {
+	msg := fmt.Sprintf(
+		`no bytes encode function for key "%s", value type "%s", and value "%v"`,
+		r.Key, r.ValueType, r.Value,
+	)
+	return msg
+}
+
+func EncodeValue(key string, valueType DataType, value any) ([]byte, error) {
+	type EncodeFunc func(any) []byte
+	returnNothing := func(any) []byte { return nil }
+	dispatchMap := map[DataType]EncodeFunc{
+		DataTypeUnknown:      returnNothing,
+		DataTypeBool:         EncodeValueBool,
+		DataTypeChar:         EncodeValueChar,
+		DataTypeInt:          EncodeValueInt,
+		DataTypeFloat:        EncodeValueFloat,
+		DataTypeString:       EncodeValueString,
+		DataTypeIntVector:    EncodeValueIntVector,
+		DataTypeFloatVector:  EncodeValueFloatVector,
+		DataTypeStringVector: EncodeValueStringVector,
+	}
+	encodeFunc, ok := dispatchMap[valueType]
+	if !ok {
+		err := ErrNoEncodeFunc{
+			Key:       key,
+			ValueType: valueType,
+			Value:     value,
+		}
+		return nil, err
+	}
+	bs := encodeFunc(value)
+	// TODO: see if `Encode` functions need to return error
+
+	return bs, nil
 }
